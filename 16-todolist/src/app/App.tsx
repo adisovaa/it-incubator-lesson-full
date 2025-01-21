6bc737f13660e593ef91f18619f67aca9f224418
adisovaa <79251934+adisovaa@users.noreply.github.com>
2025-01-20 21:39:57 -0800

    lesson 04.04 Auth

--- 16-todolist/src/app/App.tsx
+++ 16-todolist/src/app/App.tsx
@@ -1,20 +1,20 @@
 import CssBaseline from "@mui/material/CssBaseline"
-import { ThemeProvider } from "@mui/material/styles"
-import { Header, ErrorSnackbar } from "common/components"
-import { useAppSelector } from "common/hooks"
-import { getTheme } from "common/theme"
-import { selectThemeMode } from "./appSelectors"
-import { Main } from "./Main"
+import {ThemeProvider} from "@mui/material/styles"
+import {Header, ErrorSnackbar} from "common/components"
+import {useAppSelector} from "common/hooks"
+import {getTheme} from "common/theme"
+import {selectThemeMode} from "./appSelectors"
+import {Routing} from "common/routing/Rounting";
 
 export const App = () => {
-  const themeMode = useAppSelector(selectThemeMode)
+    const themeMode = useAppSelector(selectThemeMode)
 
-  return (
-    <ThemeProvider theme={getTheme(themeMode)}>
-      <CssBaseline />
-      <Header />
-      <Main />
-      <ErrorSnackbar />
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={getTheme(themeMode)}>
+            <CssBaseline/>
+            <Header/>
+            <Routing/>
+            <ErrorSnackbar/>
+        </ThemeProvider>
+    )
 }

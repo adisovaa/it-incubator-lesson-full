958164e57f1af0add9c2f473ae25cf33090b47b0
adisovaa <79251934+adisovaa@users.noreply.github.com>
2025-01-27 20:07:59 -0800

    lesson 04.04 Auth/Form/Login

--- 16-todolist/src/index.tsx
+++ 16-todolist/src/index.tsx
@@ -1,20 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import {Provider} from "react-redux"
-import {App} from "./app/App"
-import {store} from "./app/store"
-import {Login} from "./features/auth/ui/Login";
-import {BrowserRouter} from "react-router";
+import { Provider } from "react-redux"
+import { App } from "./app/App"
+import { store } from "./app/store"
+import { Login } from "./features/auth/ui/Login"
+import { BrowserRouter } from "react-router"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-            <Login/>
-        </BrowserRouter>
-    </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 )
 
 // If you want to start measuring performance in your app, pass a function
